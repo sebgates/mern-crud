cd2277cc7abb01a1f2d48cb9dfa0c7886164ebe5
Sebastian Gates <sebgates@Sebastians-MacBook-Pro.local>
2020-04-05 16:30:48 +0100

    Changed nav bar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,15 +15,9 @@ class App extends Component {
         <div className="container">
           
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <Link to="/" className="navbar-brand">MERN CRUD App</Link>
             <div className="collpase nav-collapse">
               <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
                 <li className="navbar-item">
                   <Link to="/create" className="nav-link">Create Todo</Link>
                 </li>
