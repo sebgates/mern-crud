f9037f1421b74f529646f85f29179e2d9b3cd76f
Sebastian Gates <sebgates@Sebastians-MacBook-Pro.local>
2020-04-05 16:42:01 +0100

    Changes component file names

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,24 +2,21 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import CreateTodo from "./components/create-todo.component";
-import EditTodo from "./components/edit-todo.component";
-import TodosList from "./components/todos-list.component";
-
-import logo from "./logo.png";
+import CreateTodo from "./components/create-record";
+import EditTodo from "./components/edit-record";
+import TodosList from "./components/record-list";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand">MERN CRUD App</Link>
+            <Link to="/" className="navbar-brand">MERN CRUD APP</Link>
             <div className="collpase nav-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
+                  <Link to="/create" className="nav-link">Add Record</Link>
                 </li>
               </ul>
             </div>
